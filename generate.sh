a6f05d933a673b271ac96764aa9b340ff1c16d15
Jesse Millar <jesse.millar@microsoft.com>
2020-05-07 13:06:45 -0500

    Automatic creation of the "devlog" redirect for "https://jessemillar.github.io/devlog"

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir "$1"
+mkdir "$1" || true
 sed "s,redirect-link,$2,g" template.html > "$1/index.html"
 git add -A
 git commit -m "Automatic creation of the \"$1\" redirect for \"$2\""
