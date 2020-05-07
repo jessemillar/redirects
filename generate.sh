2af8fcf126d539e90509f25fe3767322c071b018
Jesse Millar <jesse.millar@microsoft.com>
2020-05-07 14:41:54 -0500

    Automatic creation of the "spotify/party/4-3-2020" redirect for "https://open.spotify.com/playlist/1zl6faUb2RkH9eE6LwA0ae\?si\=xo3V8Cl0RRib08HiJaxeYA"

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir "$1" || true
+mkdir -p "$1" || true
 sed "s,redirect-link,$2,g" template.html > "$1/index.html"
 git add -A
 git commit -m "Automatic creation of the \"$1\" redirect for \"$2\""
