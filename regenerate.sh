f708a22336275443efca0579710fdbcc1544707e
Jesse Millar <jesse.millar@microsoft.com>
2021-06-12 11:28:55 -0500

    Don't regenerate files that don't exist

--- regenerate.sh
+++ regenerate.sh
@@ -2,6 +2,8 @@
 
 while IFS= read -r -d '' dir
 do
-	./generate.sh "$dir" "$(grep "Redirecting to" "$dir/index.html" | sed -e 's/<title>Redirecting to //g' | sed -e 's/<\/title>//g')"
+	if [ -f "$dir/index.html" ]; then
+		./generate.sh "$dir" "$(grep "Redirecting to" "$dir/index.html" | sed -e 's/<title>Redirecting to //g' | sed -e 's/<\/title>//g')"
+	fi
 done < <( find . -type d -not -path '*/.git*' -print0)
 
