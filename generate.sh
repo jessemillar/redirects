40108dc9781d02c3832af781063577084ae39bff
Jesse Millar <jesse.millar@microsoft.com>
2021-06-12 12:03:10 -0500

    Fixed the regeneration script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p "$1" || true
-sed 's,redirect-link,${2//&/\\&},g' template.html > "$1/index.html"
+sed "s,redirect-link,${2//&/\\&},g" template.html > "$1/index.html"
--- regenerate.sh
+++ regenerate.sh
@@ -4,7 +4,6 @@ while IFS= read -r -d '' dir
 do
 	if [ -f "$dir/index.html" ]; then
 		link="$(grep "Redirecting to" "$dir/index.html" | sed -e 's/<title>Redirecting to //g' | sed -e 's/<\/title>//g')"
-		echo "$link"
 		./generate.sh "$dir" "$link"
 	fi
 done < <( find . -type d -not -path '*/.git*' -print0)
