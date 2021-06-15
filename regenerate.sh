0b543bfe836caf3db1797f64120e555f0e1a1c12
Jesse Millar <jesse.millar@microsoft.com>
2021-06-15 10:33:22 -0500

    Fixing a whitespace issue

--- regenerate.sh
+++ regenerate.sh
@@ -3,7 +3,7 @@
 while IFS= read -r -d '' dir
 do
 	if [ -f "$dir/index.html" ]; then
-		link="$(grep "Redirecting to" "$dir/index.html" | sed -e 's/<title>Redirecting to //g' | sed -e 's/<\/title>//g')"
+		link="$(grep "Redirecting to" "$dir/index.html" | sed -e 's/\s*<title>Redirecting to\s*//g' | sed -e 's/<\/title>\s*//g')"
 		./generate.sh "$dir" "$link"
 	fi
 done < <( find . -type d -not -path '*/.git*' -print0)
