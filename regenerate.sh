0d5aa5a19c8997557a4145ad7913fc04c07fa991
Jesse Millar <jesse.millar@microsoft.com>
2021-06-16 10:25:32 -0500

    Attempting to ignore symlink files

--- regenerate.sh
+++ regenerate.sh
@@ -2,7 +2,7 @@
 
 while IFS= read -r -d '' dir
 do
-	if [ -f "$dir/index.html" ]; then
+	if [ -f "$dir/index.html" ] && [ ! -L "$dir/index.html" ]; then
 		link="$(grep "window.location.href" "$dir/index.html" | sed -e 's/\s*window\.location\.href = "//g' | rev | cut -c3- | rev)"
 		title="$(awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' "$dir/index.html")"
 		./generate.sh "$dir" "$link" "$title"
