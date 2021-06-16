41de9f78a2f890f744e2ee1eaa12cdce18001786
Jesse Millar <jesse.millar@microsoft.com>
2021-06-16 10:31:21 -0500

    Adding a comment

--- regenerate.sh
+++ regenerate.sh
@@ -2,6 +2,7 @@
 
 while IFS= read -r -d '' dir
 do
+	# Only work with real files, not symlinks
 	if [ -f "$dir/index.html" ] && [ ! -L "$dir/index.html" ]; then
 		link="$(grep "window.location.href" "$dir/index.html" | sed -e 's/\s*window\.location\.href = "//g' | rev | cut -c3- | rev)"
 		title="$(awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' "$dir/index.html")"
