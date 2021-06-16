e89a36e13e3d473d0a51982f1f0c04945596c709
Jesse Millar <jesse.millar@microsoft.com>
2021-06-16 10:20:42 -0500

    Finalizing grabbing page title during regeneration

--- regenerate.sh
+++ regenerate.sh
@@ -3,11 +3,9 @@
 while IFS= read -r -d '' dir
 do
 	if [ -f "$dir/index.html" ]; then
-		# TODO Parse out the page title too
 		link="$(grep "window.location.href" "$dir/index.html" | sed -e 's/\s*window\.location\.href = "//g' | rev | cut -c3- | rev)"
 		title="$(awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' "$dir/index.html")"
-		echo "$title"
-		#./generate.sh "$dir" "$link" "$title"
+		./generate.sh "$dir" "$link" "$title"
 	fi
 done < <( find . -type d -not -path '*/.git*' -print0)
 
