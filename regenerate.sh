469d69e3c7870358bc47ef287f6a57ae39ecdf73
Jesse Millar <jesse.millar@microsoft.com>
2021-06-16 10:17:32 -0500

    Moving to manual page titles

--- regenerate.sh
+++ regenerate.sh
@@ -5,7 +5,9 @@ do
 	if [ -f "$dir/index.html" ]; then
 		# TODO Parse out the page title too
 		link="$(grep "window.location.href" "$dir/index.html" | sed -e 's/\s*window\.location\.href = "//g' | rev | cut -c3- | rev)"
-		./generate.sh "$dir" "$link"
+		title="$(awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' "$dir/index.html")"
+		echo "$title"
+		#./generate.sh "$dir" "$link" "$title"
 	fi
 done < <( find . -type d -not -path '*/.git*' -print0)
 
